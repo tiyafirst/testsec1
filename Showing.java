fbb5e2a168a263dc3a83409c839fb7e55939c086
tiyafirst <gayageum5900@gmail.com>
2023-06-21 14:04:21 +0700

    Showing.java
    Showing.java added

---
+++ Showing.java
@@ -0,0 +1,11 @@
+public class Showing{
+    String message;
+
+    public Showing(){
+        this.message = "Hello";
+    }
+
+    public void Show(){
+        System.out.println(message);
+    }
+}
