145b58d2608f1bc21d767e713f2e8bb32fc8199b
tiyafirst <gayageum5900@gmail.com>
2023-06-21 14:19:59 +0700

    Create TestJoin.java

---
+++ TestJoin.java
@@ -0,0 +1,11 @@
+public class TestJoin {
+    String message;
+
+    public TestJoin() {
+        this.message = "Hello Friend";
+    }
+
+    public void Show(){
+        System.out.println(message);
+    }
+}
